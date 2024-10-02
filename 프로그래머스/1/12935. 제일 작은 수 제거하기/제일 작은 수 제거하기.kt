28fb5143bc7535c361935c277fd1a8054d30d058
Jerry <86874699+roswkzm@users.noreply.github.com>
2024-10-03 02:42:32 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 28.49 ms, Memory: 63.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        val minValue = arr.minOrNull() ?: return intArrayOf(-1)
+        val result = arr.filter { it != minValue }.toIntArray()
+        return if (result.isEmpty()) intArrayOf(-1) else result
+    }
+}
