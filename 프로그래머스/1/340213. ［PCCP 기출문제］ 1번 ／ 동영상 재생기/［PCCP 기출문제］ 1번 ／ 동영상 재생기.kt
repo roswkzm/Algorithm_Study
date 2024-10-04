62c22f28743a0bca25a74b3cc0ec4d6996e6c04c
Jerry <86874699+roswkzm@users.noreply.github.com>
2024-10-05 06:30:08 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 21.39 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.kt
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.kt
@@ -1,45 +1,48 @@
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-
 class Solution {
-    fun solution(
-        video_len: String,
-        pos: String,
-        op_start: String,
-        op_end: String,
-        commands: Array<String>
-    ): String {
-        val suffixHour = "00:"
-        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-        var currentPos = LocalTime.parse(suffixHour + pos, timeFormatter)
-        val videoLen = LocalTime.parse(suffixHour + video_len, timeFormatter)
-        val opStart = LocalTime.parse(suffixHour + op_start, timeFormatter)
-        val opEnd = LocalTime.parse(suffixHour + op_end, timeFormatter)
-        val videoStart = LocalTime.parse(suffixHour + "00:00", timeFormatter)
+    fun solution(video_len: String, pos: String, op_start: String, op_end: String, commands: Array<String>): String {
 
-        if (currentPos >= opStart && currentPos < opEnd) {
+        // "mm:ss" 문자열을 초로 변환하는 함수
+        fun toSeconds(time: String): Int {
+            val (mm, ss) = time.split(":").map { it.toInt() }
+            return mm * 60 + ss
+        }
+
+        // 초를 "mm:ss" 문자열로 변환하는 함수
+        fun toMMSS(seconds: Int): String {
+            val mm = seconds / 60
+            val ss = seconds % 60
+            return "%02d:%02d".format(mm, ss)
+        }
+
+        // 주어진 값들을 초로 변환
+        val videoLen = toSeconds(video_len)
+        var currentPos = toSeconds(pos)
+        val opStart = toSeconds(op_start)
+        val opEnd = toSeconds(op_end)
+
+        // 오프닝 구간에 있을 경우 오프닝 끝으로 이동
+        if (currentPos in opStart..opEnd) {
             currentPos = opEnd
         }
 
+        // 명령어 처리
         for (command in commands) {
             when (command) {
-                "next" -> {
-                    if (currentPos.plusSeconds(10) > videoLen) currentPos =
-                        videoLen else currentPos = currentPos.plusSeconds(10)
-                }
-
                 "prev" -> {
-                    if (currentPos.minusSeconds(10) > currentPos) currentPos =
-                        videoStart else currentPos = currentPos.minusSeconds(10)
+                    currentPos = maxOf(0, currentPos - 10) // 10초 전으로 이동, 최소 0초
+                }
+                "next" -> {
+                    currentPos = minOf(videoLen, currentPos + 10) // 10초 후로 이동, 최대 동영상 끝
                 }
             }
 
-            if (currentPos >= opStart && currentPos < opEnd) {
+            // 오프닝 구간에 있을 경우 오프닝 끝으로 이동
+            if (currentPos in opStart..opEnd) {
                 currentPos = opEnd
             }
         }
 
-        val mmssFormatter = DateTimeFormatter.ofPattern("mm:ss")
-        return currentPos.format(mmssFormatter)
+        // 결과를 "mm:ss" 형식으로 변환하여 반환
+        return toMMSS(currentPos)
     }
 }
