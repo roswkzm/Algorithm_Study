fe6eecf916699074b8bdcbf971abc522d857028e
Jerry <86874699+roswkzm@users.noreply.github.com>
2024-11-01 02:27:13 +0900

    [level 2] Title: 배달, Time: 33.15 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.kt
+++ 프로그래머스/2/12978. 배달/배달.kt
@@ -1,26 +1,28 @@
+import java.util.PriorityQueue
+
 class Solution {
+    // N개의 마을 중에서 K 시간 이하로 배달이 가능한 마을에서만 주문을 받으려고 합니다.
     fun solution(N: Int, road: Array<IntArray>, k: Int): Int {
         val roadMap = road.flatMap { listOf(it[0] to Pair(it[1], it[2]), it[1] to Pair(it[0], it[2])) }
             .groupBy({ it.first }, { it.second.first to it.second.second })
-        val visited = IntArray(N) { Int.MAX_VALUE }
-        val queue = ArrayDeque<Pair<Int, Int>>()
+        val distances = IntArray(N) { Int.MAX_VALUE }
 
-        queue.add(1 to 0)
-        visited[0] = 0
+        val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+        pq.add(1 to 0)
+        distances[0] = 0
 
-        while (queue.isNotEmpty()) {
-            val (currentVillage, currentCost) = queue.removeFirst()
+        while (pq.isNotEmpty()) {
+            val (currentVillage, currentCost) = pq.poll()!!
 
             for ((nextVillage, nextCost) in roadMap.getValue(currentVillage)) {
                 val nextVillageCost = currentCost + nextCost
-                if (nextVillageCost < visited[nextVillage - 1] && currentCost + nextCost <= k) {
-                    queue.add(nextVillage to nextVillageCost)
-                    visited[nextVillage - 1] = nextVillageCost
+                if (nextVillageCost < distances[nextVillage - 1]) {
+                    pq.offer(nextVillage to nextVillageCost)
+                    distances[nextVillage - 1] = nextVillageCost
                 }
-
             }
         }
 
-        return visited.count{ it <= k }
+        return distances.count { it <= k }
     }
 }
