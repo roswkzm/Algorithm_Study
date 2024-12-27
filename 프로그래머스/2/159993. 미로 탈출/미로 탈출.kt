699d97bcf479cf6396eae1f4da65acdc6d23abc4
Jerry <86874699+roswkzm@users.noreply.github.com>
2024-12-27 17:06:20 +0900

    [level 2] Title: 미로 탈출, Time: 56.06 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.kt
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.kt
@@ -1,53 +1,48 @@
 class Solution {
     fun solution(maps: Array<String>): Int {
+        var answer: Int = 0
         val moveList = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
         var startIndex = Pair(0, 0)
         var leverIndex = Pair(0, 0)
-        var exitIndex = Pair(0, 0)
-        maps.forEachIndexed { row, line ->
-            line.forEachIndexed { col, char ->
-                when (char) {
-                    'S' -> startIndex = row to col
-                    'L' -> leverIndex = row to col
-                    'E' -> exitIndex = row to col
+        var endIndex = Pair(0, 0)
+
+        maps.forEachIndexed { rowIndex, s ->
+            s.forEachIndexed { colIndex, c ->
+                when (c) {
+                    'S' -> startIndex = rowIndex to colIndex
+                    'L' -> leverIndex = rowIndex to colIndex
+                    'E' -> endIndex = rowIndex to colIndex
                 }
             }
         }
 
-        fun bfs(start: Pair<Int, Int>, destination: Pair<Int, Int>): Int {
+        fun bfs(origin: Pair<Int, Int>, destination: Pair<Int, Int>): Int? {
             val queue = ArrayDeque<Triple<Int, Int, Int>>()
             val visited = Array(maps.size) { BooleanArray(maps[0].length) }
-            queue.add(Triple(start.first, start.second, 0))
-            visited[start.first][start.second] = true
+            queue.add(Triple(origin.first, origin.second, 0))
+            visited[origin.first][origin.second] = true
 
             while (queue.isNotEmpty()) {
-                val (row, column, length) = queue.removeFirst()
-                if (row == destination.first && column == destination.second) {
-                    return length
+                val (row, column, time) = queue.removeFirst()
+                if (destination == row to column) {
+                    return time
                 }
 
                 for ((dr, dc) in moveList) {
                     val nextRow = row + dr
                     val nextColumn = column + dc
-                    if (nextRow in maps.indices && nextColumn in maps[0].indices &&
-                        maps[nextRow][nextColumn] != 'X' && !visited[nextRow][nextColumn]) {
-                        queue.add(Triple(nextRow, nextColumn, length + 1))
+                    if (nextRow in maps.indices && nextColumn in maps[0].indices && maps[nextRow][nextColumn] != 'X' && !visited[nextRow][nextColumn]) {
+                        queue.add(Triple(nextRow, nextColumn, time + 1))
                         visited[nextRow][nextColumn] = true
                     }
                 }
             }
-            return -1
+            return null
         }
 
-        val toLever = bfs(startIndex, leverIndex)
-        if (toLever == -1) return -1
+        answer += bfs(startIndex, leverIndex) ?: return -1
+        answer += bfs(leverIndex, endIndex) ?: return -1
 
-        val toExit = bfs(leverIndex, exitIndex)
-        if (toExit == -1) return -1
-
-        println(toLever)
-        println(toExit)
-
-        return toLever + toExit
+        return answer
     }
 }
