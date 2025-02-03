e845fc3d6d9893db342d5f9ee4756f6a568278c4
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-03 17:51:36 +0900

    [level 2] Title: 피보나치 수, Time: 35.01 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
@@ -1,9 +1,7 @@
 class Solution {
     fun solution(n: Int): Int {
-        val answer = arrayListOf(0, 1)
-        // 3부터 n까지 반복하며 피보나치 수를 배열에 저장해 줍니다.
-        (2..n).forEach{ answer.add((answer[it - 2] + answer[it - 1]) % 1234567) }
-        // 배열에 n번 칸에 들어있는 숫자를 반환합니다.
+        var answer = arrayListOf(0,1)
+        (2..n).forEach{answer.add((answer[it - 1] + answer[it -2]) % 1234567)}
         return answer[n]
     }
 }
