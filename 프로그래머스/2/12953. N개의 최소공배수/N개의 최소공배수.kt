e65643c952d443f3358d9b8590e28d49f3da2439
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-03 18:12:05 +0900

    [level 2] Title: N개의 최소공배수, Time: 19.38 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun solution(arr: IntArray): Int {
-        return arr.reduce { acc, i -> lcm(acc, i) }
+        return arr.reduce{ acc, i -> lcm(acc, i)}
     }
-
-    private fun gcb(a: Int, b: Int): Int {
-        return if (b == 0) a else gcb(b, (a % b))
+    
+    private fun gcb(a : Int, b : Int) : Int {
+        return if(b == 0) a else gcb(b, (a % b))
     }
-
-    private fun lcm(a: Int, b: Int): Int {
+    
+    private fun lcm(a: Int, b : Int) : Int {
         return (a * b) / gcb(a, b)
     }
 }
