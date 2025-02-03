07ade2306b0736beb292ff1cb8a4f48c32b6c892
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-03 18:22:59 +0900

    [level 2] Title: 귤 고르기, Time: 142.93 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -1,19 +1,18 @@
 class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
         var answer: Int = 0
-        var addCount = 0
-
-        // 귤 Size를 Key값으로 갖고 개수를 Value로 갖는 Map을 만들자.
-        val tangerineCountMap = tangerine.groupBy { it }.map { it.key to it.value.count() }
-        val sortList = tangerineCountMap.map { it.second }.sortedDescending()
-
-        for (i in sortList) {
-            if (addCount < k) {
-                addCount += i
+        var currentTangerine = 0
+        
+        val tangerineSortCount = tangerine.groupBy{ it }.map{ it.key to it.value.count() }
+        .map { it.second }.sortedDescending()
+        
+        for(i in tangerineSortCount){
+            if(currentTangerine < k){
                 answer++
+                currentTangerine += i
             }
         }
-
+        
         return answer
     }
 }
