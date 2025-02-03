66532c3c263f29a31e1e23a0cd6ac70fe19d5ad2
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-03 22:59:01 +0900

    [level 2] Title: 괄호 회전하기, Time: 35.20 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -1,36 +1,33 @@
-import java.util.Stack
+import java.util.*
 
 class Solution {
     fun solution(s: String): Int {
-        var validCount = 0
-
-        // s의 길이만큼 회전
-        for (i in s.indices) {
-            val rotatedS = s.substring(i) + s.substring(0, i)
-            if (isValid(rotatedS)) {
-                validCount++
-            }
+        var answer: Int = 0
+        
+        for(i in s.indices){
+            val rotateS = s.substring(i) + s.substring(0, i)
+            if(isCorrect(rotateS)) answer++
         }
-
-        return validCount
+        
+        return answer
     }
-
-    // 올바른 괄호 문자열인지 확인하는 함수
-    private fun isValid(str: String): Boolean {
+    
+    private fun isCorrect(s : String) : Boolean {
         val stack = Stack<Char>()
-        for (char in str) {
-            when (char) {
-                '(', '[', '{' -> stack.push(char)
-                ')' -> {
-                    if (stack.isEmpty() || stack.pop() != '(') return false
+        
+        for(c in s){
+            when(c) {
+                '[', '(', '{' -> {
+                    stack.add(c)
                 }
-
                 ']' -> {
-                    if (stack.isEmpty() || stack.pop() != '[') return false
+                    if(stack.isEmpty() || stack.pop() != '[') return false
                 }
-
+                ')' -> {
+                    if(stack.isEmpty() || stack.pop() != '(') return false
+                }
                 '}' -> {
-                    if (stack.isEmpty() || stack.pop() != '{') return false
+                    if(stack.isEmpty() || stack.pop() != '{') return false
                 }
             }
         }
