055641520c149613965850d0cc912e3f59a78e53
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-04 23:43:13 +0900

    [level 2] Title: 튜플, Time: 121.07 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.kt
+++ 프로그래머스/2/64065. 튜플/튜플.kt
@@ -1,15 +1,18 @@
 class Solution {
     fun solution(s: String): IntArray {
-        val answer = mutableSetOf<Int>()
-        val result = s.substring(2, s.length - 2).split("},{")
-            .map { it.split(",").map { it.toInt() } }.sortedBy { it.size }
-
-        for (set in result) {
-            for (num in set) {
-                answer.add(num)
+        var answer = mutableSetOf<Int>()
+        val result = s.substring(2, s.length -2).split("},{").map{ 
+            it.split(",").map{ 
+                it.toInt()
+            }
+        }.sortedBy{ it.size }
+        
+        for(list in result){
+            for(i in list){
+                answer.add(i)
             }
         }
-
+        
         return answer.toIntArray()
     }
 }
