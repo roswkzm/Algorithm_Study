7f85253c747daee043fb57777b273ec26f49d239
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-06 06:04:50 +0900

    [level 2] Title: 소수 찾기, Time: 37.48 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.kt
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.kt
@@ -2,33 +2,36 @@ import kotlin.math.sqrt
 
 class Solution {
     fun solution(numbers: String): Int {
-        val makeWordList = mutableSetOf<String>()
-
-        fun backtrack(word: String, userIndex: MutableList<Int>) {
-            if (word.isNotEmpty() && word.toInt() > 1 && word.first() != '0') {
-                if (isPrime(word.toInt())) {
-                    makeWordList.add(word)
+        var answer = mutableSetOf<String>()
+        val numberList = numbers.map{ it.digitToInt() }
+        
+        fun backtrack(indexList : MutableList<Int>, word : String) {
+            if(word.isNotEmpty() && word.toInt() > 1 && word.first() != '0'){
+                if(isPrime(word.toInt())){
+                    answer.add(word)
                 }
             }
-            for (i in numbers.indices) {
-                if (i !in userIndex) {
-                    userIndex.add(i)
-                    backtrack(word + numbers[i], userIndex)
-                    userIndex.removeAt(userIndex.size - 1)
+            
+            for(i in numbers.indices) {
+                if(i !in indexList){
+                    indexList.add(i)
+                    backtrack(indexList, word + numbers[i])
+                    indexList.removeLast()
                 }
             }
         }
-
-        backtrack("", mutableListOf())
-        return makeWordList.count()
+        
+        backtrack(mutableListOf(), "")
+        
+        return answer.size
     }
-
-    private fun isPrime(num: Int): Boolean {
-        if (num < 2) return false
-
-        for (i in 2..sqrt(num.toDouble()).toInt()) {
-            if (num % i == 0) return false
+    
+    private fun isPrime(n : Int) : Boolean {
+        if(n < 2) return false
+        for(i in (2..sqrt(n.toDouble()).toInt())) {
+            if(n % i == 0) return false 
         }
+        
         return true
     }
 }
