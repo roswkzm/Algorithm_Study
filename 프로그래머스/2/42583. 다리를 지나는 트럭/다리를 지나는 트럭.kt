e336b0e87e1e3ffef14b7eecbb1bfb72b34cd456
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-06 07:05:54 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 164.93 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -3,29 +3,26 @@ class Solution {
         var answer = 0
         val queue = ArrayDeque<Pair<Int, Int>>()
         val truckList = truck_weights.toMutableList()
-
+        
         queue.add(truckList.removeFirst() to 0)
-
-        while (queue.isNotEmpty()) {
-            // 시간초 증가
+        
+        while(queue.isNotEmpty()) {
             answer++
-            // 현재 다리위에 있는 트럭들 무게
-            var currentBridgeWeight = queue.sumOf { it.first }
-            // 다리위의 트럭들 1칸씩 전진
-            queue.forEachIndexed { index, pair ->
-                queue[index] = pair.first to (pair.second + 1)
+            queue.forEachIndexed{index, pair -> 
+                queue[index] = pair.first to pair.second + 1
             }
-            // 맨 앞의 트럭이 bridge_length랑 똑같다면 다 건넌거다.
-            if (queue.first().second > bridge_length) {
+            
+            if(queue.first().second > bridge_length){
                 queue.removeFirst()
-                currentBridgeWeight = queue.sumOf { it.first }
             }
-            // 다음트럭이 건널 수 있다면 건너라
-            if (truckList.isNotEmpty() && currentBridgeWeight + truckList.first() <= weight) {
+            
+            val totalWeight = queue.sumOf{ it.first }
+            
+            if(truckList.isNotEmpty() && totalWeight + truckList.first() <= weight && queue.size < bridge_length){
                 queue.add(truckList.removeFirst() to 1)
             }
         }
-
+        
         return answer
     }
 }
