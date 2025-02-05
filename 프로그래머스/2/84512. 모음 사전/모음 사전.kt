3ca6bbe814f4e9cb4dcb3bb55066c2372999812c
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-05 21:16:12 +0900

    [level 2] Title: 모음 사전, Time: 35.30 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.kt
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.kt
@@ -1,22 +1,22 @@
 class Solution {
     fun solution(word: String): Int {
         val alphabet = listOf("A", "E", "I", "O", "U")
-        val wordMakeList = mutableListOf<String>()
+        val makeWordList = mutableListOf<String>()
 
-        fun backtrack(current: String) {
-            if (current.length > alphabet.size) {
-                return
-            }
-            if (current.isNotEmpty()) {
-                wordMakeList.add(current)
-            }
+        fun backtrack(s : String) {
+            if(s.length > 5) return
 
-            for (c in alphabet) {
-                backtrack(current + c)
+            if(s.isNotEmpty()) makeWordList.add(s)
+
+            for(a in alphabet) {
+                backtrack(s + a)
             }
         }
 
         backtrack("")
-        return wordMakeList.indexOf(word) + 1
+
+        println(makeWordList)
+
+        return makeWordList.indexOf(word) + 1
     }
 }
