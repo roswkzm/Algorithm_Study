61f656e6cdd4a546f180323ef95f4df1de370f9f
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-05 23:34:18 +0900

    [level 2] Title: 택배상자, Time: 111.09 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.kt
+++ 프로그래머스/2/131704. 택배상자/택배상자.kt
@@ -1,34 +1,27 @@
-import java.util.Stack
+import java.util.*
 
 class Solution {
     fun solution(order: IntArray): Int {
         val subContainer = Stack<Int>()
         var currentBox = 1
-        var idx = 0
-
-        // 내 상자개수가 택배기사가 원하는 개수랑 같거나 작을때 동안
-        while (currentBox <= order.size){
-            // 기사가 원하는게 내 손에 있을경우
-            if (order[idx] == currentBox){
-                idx++
-            } else {    // 기사가 원하는게 내손에 없다면 보조 컨테이너에 추가
-                subContainer.push(currentBox)
+        var index = 0
+        
+        while(currentBox <= order.size){
+            
+            if(currentBox == order[index]){
+                index++
+            } else {
+                subContainer.add(currentBox)
             }
-
-            // 보조 컨테이너가 비어있지 안거나 거기에 기사가 원하는 상자를 바로 꺼낼수 있다면
-            while (subContainer.isNotEmpty() && subContainer.peek() == order[idx]){
-                subContainer.pop()
-                idx++
+            
+            while(subContainer.isNotEmpty() && subContainer.last() == order[index]){
+                subContainer.removeLast()
+                index++
             }
-            // 다음상자로 이동
+            
             currentBox++
         }
-        // 남은상자 처리가능하다면 처리
-        while (subContainer.isNotEmpty() && subContainer.peek() == order[idx]){
-            subContainer.pop()
-            idx++
-        }
-
-        return idx
+        
+        return index
     }
 }
