153b1646c9c634796e62feb76a3a7e4d4b464218
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-06 04:34:08 +0900

    [level 2] Title: 오픈채팅방, Time: 194.23 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.kt
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.kt
@@ -1,26 +1,26 @@
 class Solution {
     fun solution(record: Array<String>): Array<String> {
-        val answer = mutableListOf<String>()
+        var answer = mutableListOf<String>()
         val nameMap = mutableMapOf<String, String>()
-
-        record.forEach {
-            val (action, userId) = it.split(" ")
-            val userName = it.split(" ").getOrNull(2)
-            if (userName != null) {
-                nameMap[userId] = userName
+        
+        record.forEach{
+            val (_, userId) = it.split(" ")
+            val nickName = it.split(" ").getOrNull(2)
+            if(nickName != null) {
+            nameMap[userId] = nickName
             }
         }
-
-        record.forEach {
-            val (action, userId) = it.split(" ")
-            val userName = nameMap.getValue(userId)
-            if (action == "Enter") {
-                answer.add("${userName}님이 들어왔습니다.")
-            } else if (action == "Leave") {
-                answer.add("${userName}님이 나갔습니다.")
+        
+        record.forEach{
+            val (task, userId) = it.split(" ")
+            val currNickName = nameMap.getValue(userId)
+            if(task == "Enter"){
+                answer.add("${currNickName}님이 들어왔습니다.")
+            } else if(task == "Leave"){
+                answer.add("${currNickName}님이 나갔습니다.")
             }
         }
-
+        
         return answer.toTypedArray()
     }
 }
