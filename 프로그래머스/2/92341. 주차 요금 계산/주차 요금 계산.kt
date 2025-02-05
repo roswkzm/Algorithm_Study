c398746b32c07c6b71a2ca30a2f37be91aa4b450
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-06 03:24:54 +0900

    [level 2] Title: 주차 요금 계산, Time: 28.03 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.kt
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.kt
@@ -1,50 +1,42 @@
-import kotlin.math.ceil
+import kotlin.math.*
 
 class Solution {
     fun solution(fees: IntArray, records: Array<String>): IntArray {
+        var answer: IntArray = intArrayOf()
+        val inOutTimeMap = mutableMapOf<String, Int>()
         val parkingTimeMap = mutableMapOf<String, Int>()
-        val inTimeMap = mutableMapOf<String, Int>()
 
-        // 시각을 분으로 변환하는 함수
-        fun timeToMinutes(time: String): Int {
-            val (hour, minute) = time.split(":").map { it.toInt() }
-            return hour * 60 + minute
-        }
-
-        // 주차 요금을 계산하는 함수
-        fun calculateFee(totalTime: Int, fees: IntArray): Int {
-            val (baseTime, baseFee, unitTime, unitFee) = fees
-            return if (totalTime <= baseTime) {
-                baseFee
-            } else {
-                baseFee + ceil((totalTime - baseTime) / unitTime.toDouble()).toInt() * unitFee
-            }
-        }
-
-        // 기록을 하나씩 처리
         for (record in records) {
-            val (time, carNumber, action) = record.split(" ")
-            val minutes = timeToMinutes(time)
+            val (time, number, type) = record.split(" ")
+            val minute = timeToMinute(time)
 
-            if (action == "IN") {
-                inTimeMap[carNumber] = minutes
-            } else if (action == "OUT") {
-                val inTime = inTimeMap.remove(carNumber)!!
-                parkingTimeMap[carNumber] =
-                    parkingTimeMap.getOrDefault(carNumber, 0) + (minutes - inTime)
+            if (type == "IN") {
+                inOutTimeMap[number] = minute
+            } else {
+                val inTime = inOutTimeMap.remove(number)!!
+                parkingTimeMap[number] = parkingTimeMap.getOrDefault(number, 0) + (minute - inTime)
             }
         }
 
-        // 입차된 채 출차 기록이 없는 차량을 23:59에 출차한 것으로 간주
-        val closingTime = timeToMinutes("23:59")
-        for ((carNumber, inTime) in inTimeMap) {
-            parkingTimeMap[carNumber] =
-                parkingTimeMap.getOrDefault(carNumber, 0) + (closingTime - inTime)
+        val closedTime = timeToMinute("23:59")
+        for ((number, inTime) in inOutTimeMap){
+            parkingTimeMap[number] = parkingTimeMap.getOrDefault(number, 0) + (closedTime - inTime)
         }
 
-        // 차량 번호 순으로 정렬하고 요금을 계산하여 결과 배열로 반환
-        return parkingTimeMap.keys.sorted().map { carNumber ->
-            calculateFee(parkingTimeMap[carNumber]!!, fees)
-        }.toIntArray()
+        return parkingTimeMap.toSortedMap().map{ calculationFee(it.value, fees) }.toIntArray()
+    }
+    
+    private fun calculationFee(totalTime : Int, fees: IntArray): Int {
+        val (defaultMinute, defaultFee, unitMinute, unitFee) = fees
+        return if(totalTime <= defaultMinute) {
+            defaultFee
+        } else {
+            defaultFee + ceil((totalTime - defaultMinute) / unitMinute.toDouble()).toInt() * unitFee
+        }
+    }
+
+    private fun timeToMinute(s: String): Int {
+        val (hour, minute) = s.split(":").map { it.toInt() }
+        return hour * 60 + minute
     }
 }
