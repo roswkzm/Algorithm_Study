15644a03aca50ac1189553089c38b720ca0060d3
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-05 18:01:20 +0900

    [level 2] Title: 타겟 넘버, Time: 7.80 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,19 +1,21 @@
 class Solution {
     fun solution(numbers: IntArray, target: Int): Int {
         var answer = 0
-
-        fun dfs(index : Int, currentSum : Int){
-            if (index == numbers.size){
-                if (currentSum == target) answer++
+        
+        fun dfs(index : Int, currentSum : Int) {
+            if(index == numbers.size){
+                if(currentSum == target) {
+                    answer++
+                }
                 return
             }
-
+            
             dfs(index + 1, currentSum + numbers[index])
             dfs(index + 1, currentSum - numbers[index])
         }
-
+        
         dfs(0, 0)
-
+        
         return answer
     }
 }
