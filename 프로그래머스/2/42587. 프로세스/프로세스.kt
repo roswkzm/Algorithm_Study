e6e622727b9226719da9beb3e69fe16954dc6699
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-05 17:54:16 +0900

    [level 2] Title: 프로세스, Time: 4.22 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.kt
+++ 프로그래머스/2/42587. 프로세스/프로세스.kt
@@ -1,19 +1,24 @@
+import java.util.*
+
 class Solution {
     fun solution(priorities: IntArray, location: Int): Int {
         var answer = 0
-
+        val locationPriorities = priorities.mapIndexed{ index, i -> index to i}
         val queue = ArrayDeque<Pair<Int, Int>>()
-        queue.addAll(priorities.mapIndexed { index, priority -> priority to index })
+        queue.addAll(locationPriorities)
 
-        while (queue.isNotEmpty()) {
-            val curProcess = queue.removeFirst()
-            if (queue.any { it.first > curProcess.first }) {
-                queue.add(curProcess)
+        while (queue.isNotEmpty()){
+            val currTask = queue.pop()
+
+            if (queue.any { it.second > currTask.second }){
+                queue.add(currTask)
+                continue
             } else {
-                answer++
-                if (curProcess.second == location) {
-                    return answer
-                }
+                answer ++
+            }
+
+            if (currTask.first == location) {
+                return answer
             }
         }
 
