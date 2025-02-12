665fd06454041421441788f3a458a0a09994a4f9
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-13 06:50:04 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 21204 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (A, B) = br.readLine().split(" ").map{ it.toInt() }
+    println(A + B)
+    println(A - B)
+    println(A * B)
+    println(A / B)
+    println(A % B)
+}
