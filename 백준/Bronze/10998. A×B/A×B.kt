4423cdb54787ca75fd77a513934f324e640c5322
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-13 06:44:04 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 21300 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (A, B) = br.readLine().split(" ").map{ it.toInt() }
+    print(A * B)
+}
