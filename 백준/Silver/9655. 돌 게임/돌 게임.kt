9f4f4e672d1b768b86d3be02bfa358e20ecd04b0
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-17 22:00:46 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 12212 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    println(if (n % 2 == 0) "CY" else "SK")
+}
