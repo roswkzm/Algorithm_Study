e8b43f55046e1026496e793ba52f8816885935cf
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-20 02:22:21 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 112 ms, Memory: 21160 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    println(n * m - 1)
+}
