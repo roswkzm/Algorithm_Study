ff5494d375309601662dbe84d9dacaab6f4d03ea
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-02-24 05:01:37 +0900

    [Bronze I] Title: ROT13, Time: 112 ms, Memory: 21296 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val s = br.readLine().map { it }
+
+    val result = s.map {
+        when (it) {
+            in 'A'..'Z' -> ((it - 'A' + 13) % 26 + 'A'.code).toChar()
+            in 'a'..'z' -> ((it - 'a' + 13) % 26 + 'a'.code).toChar()
+            else -> it
+        }
+    }
+
+    println(result.joinToString(""))
+}
