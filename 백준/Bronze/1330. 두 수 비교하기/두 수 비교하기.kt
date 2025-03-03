3d1e98566d1d89280be127c72396fd15bc898f72
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-03 17:41:43 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 112 ms, Memory: 20944 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+
+    when {
+        a > b -> println(">")
+        a < b -> println("<")
+        a == b -> println("==")
+    }
+}
