c312a95d57e01233b6491e1c2792ce541c6d9123
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-04 21:13:18 +0900

    [Bronze III] Title: 최소, 최대, Time: 544 ms, Memory: 159496 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val list = br.readLine().split(" ").map { it.toInt() }
+    println("${list.minOf { it }} ${list.maxOf { it }}")
+}
