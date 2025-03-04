86bd97252e21f5210168277dd222eab5e0044fc2
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-04 20:53:49 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 96 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    println("long ".repeat(n / 4) + "int")
+}
