98085f0cedb4f143afda88ee29bba2334d645937
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-04 21:01:15 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 21256 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val list = br.readLine().split(" ").map { it.toInt() }
+    val findNumber = br.readLine().toInt()
+    println(list.count { it == findNumber })
+}
