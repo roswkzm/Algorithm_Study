c9a9e9c56bcd9ffaf966c12eda28abd8f66f0b46
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-05 22:30:46 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 108 ms, Memory: 15180 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val n = readLine()!!.toInt()
+
+    for (i in 1..n) {
+        println(" ".repeat(n - i) + "*".repeat(2 * i - 1))
+    }
+
+    for (i in n - 1 downTo 1) {
+        println(" ".repeat(n - i) + "*".repeat(2 * i - 1))
+    }
+}
