7fa3a568aee2e72947498fb04c624e409a68d29c
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-15 03:06:24 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 164 ms, Memory: 21464 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val numbers = readLine()!!.split(" ").map { it.toInt() }
+
+    for (x in -999..999) {
+        for (y in -999..999) {
+            if (
+                (numbers[0] * x) + (numbers[1] * y) == numbers[2] &&
+                (numbers[3] * x) + (numbers[4] * y) == numbers[5]
+            ) {
+                println("$x $y")
+                return
+            }
+        }
+    }
+}
