eb82c05d07c8ea9ff41105878bad79b796956fac
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-18 06:17:17 +0900

    [Silver III] Title: 소수 구하기, Time: 964 ms, Memory: 38040 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.kt
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.kt
@@ -13,8 +13,8 @@ fun isPrime(n: Int): Boolean {
     if (n == 2 || n == 3) return true
     if (n % 2 == 0 || n % 3 == 0) return false
     val sqrtN = sqrt(n.toDouble()).toInt()
-    for (i in 5..sqrtN) {
-        if (n % i == 0) return false
+    for (i in 5..sqrtN step 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false
     }
     return true
 }
