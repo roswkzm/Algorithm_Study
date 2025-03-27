d6e0cc133d7cc6bf881f274de6a93b6fcf37a146
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-03-27 20:14:28 +0900

    [level 3] Title: 가장 먼 노드, Time: 535.27 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.kt
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.kt
@@ -1,36 +1,31 @@
 class Solution {
     fun solution(n: Int, edge: Array<IntArray>): Int {
         val graph = mutableMapOf<Int, MutableList<Int>>()
-        edge.forEach {
-            val from = it[0]
-            val to = it[1]
-            graph.computeIfAbsent(from) { mutableListOf() }.add(to)
-            graph.computeIfAbsent(to) { mutableListOf() }.add(from)
+        for ((a, b) in edge) {
+            graph.computeIfAbsent(a) { mutableListOf() }.add(b)
+            graph.computeIfAbsent(b) { mutableListOf() }.add(a)
         }
+        val costs = IntArray(n)
+        val queue = ArrayDeque<Pair<Int, Int>>()
+        val visited = BooleanArray(n)
 
-        fun bfs(): Int {
-            val visited = BooleanArray(n)
-            val cost = IntArray(n)
-            val queue = ArrayDeque<Pair<Int, Int>>()
+        queue.add(1 to 0)
+        costs[0] = 0
+        visited[0] = true
 
-            queue.add(1 to 0)
-            visited[0] = true
-            cost[0] = 0
+        while (queue.isNotEmpty()) {
+            val (node, cost) = queue.removeFirst()
 
-            while (queue.isNotEmpty()) {
-                val (node, currCost) = queue.removeFirst()
-                for (nextNode in graph.getOrDefault(node, mutableListOf())) {
-                    val nextNodeIndex = nextNode - 1
-                    if (!visited[nextNodeIndex]) {
-                        queue.add(nextNode to currCost + 1)
-                        visited[nextNodeIndex] = true
-                        cost[nextNodeIndex] = currCost + 1
-                    }
+            for (nextNode in graph.getValue(node)) {
+                val nextNodeIndex = nextNode - 1
+                if (!visited[nextNodeIndex]) {
+                    queue.add(nextNode to cost + 1)
+                    costs[nextNodeIndex] = cost + 1
+                    visited[nextNodeIndex] = true
                 }
             }
-
-            return cost.count { it == cost.maxOf { it } }
         }
-        return bfs()
+
+        return costs.count { it == costs.maxOf { it } }
     }
 }
