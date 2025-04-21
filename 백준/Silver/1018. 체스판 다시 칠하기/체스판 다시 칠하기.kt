ce294364d20125be920e25b37f52be21d21d8178
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-04-21 18:02:45 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 140 ms, Memory: 22380 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.kt
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.kt
@@ -1,31 +1,28 @@
 fun main() {
     val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-    val board = Array(n) { readLine()!!.map { it } }
-    var minWrongColor = Int.MAX_VALUE
+    val board = Array(n) { readLine()!! }
 
-    for (i in 0..m - 8) {
-        for (j in 0..n - 8) {
-            val newBoard = board.slice(j until j + 8).map { it.slice(i until i + 8) }
-            minWrongColor = minOf(minWrongColor, boardMinWrongCount(newBoard))
+    var minPaint = Int.MAX_VALUE
+
+    for (i in 0..n - 8) {
+        for (j in 0..m - 8) {
+            var count1 = 0
+            var count2 = 0
+
+            for (x in 0 until 8) {
+                for (y in 0 until 8) {
+                    val current = board[i + x][j + y]
+                    val expected1 = if ((x + y) % 2 == 0) 'W' else 'B'
+                    val expected2 = if ((x + y) % 2 == 0) 'B' else 'W'
+
+                    if (current != expected1) count1++
+                    if (current != expected2) count2++
+                }
+            }
+
+            minPaint = minOf(minPaint, count1, count2)
         }
     }
 
-    println(minWrongColor)
-}
-
-fun boardMinWrongCount(newBoard: List<List<Char>>): Int {
-    val boardColor = mutableListOf('B', 'W')
-    var correctSum = 0
-
-    for (col in 0 until 8) {
-        val colStartChar = boardColor[col % 2]
-        correctSum += newBoard[col].filterIndexed { index, c ->
-            if (index % 2 == 0) {
-                c == colStartChar
-            } else {
-                c != colStartChar
-            }
-        }.count()
-    }
-    return minOf(correctSum, 64 - correctSum)
+    println(minPaint)
 }
