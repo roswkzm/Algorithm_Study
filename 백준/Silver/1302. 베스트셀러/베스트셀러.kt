afbd0d1683b4108909edc1bc90e1dedff977795b
Jerry <86874699+roswkzm@users.noreply.github.com>
2025-05-23 18:38:12 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.kt
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.kt
@@ -1,13 +1,13 @@
 fun main() {
-    val n = readLine()!!.toInt()
-    val map = mutableMapOf<String, Int>()
+    val n = readln().toInt()
+    val bookCount = mutableMapOf<String, Int>()
 
     repeat(n) {
-        val title = readLine()!!
-        map[title] = map.getOrDefault(title, 0) + 1
+        val title = readln()
+        bookCount[title] = bookCount.getOrDefault(title, 0) + 1
     }
 
-    val max = map.values.maxOrNull()!!
-    val candidates = map.filterValues { it == max }.keys
-    println(candidates.minOrNull())
+    val maxCount = bookCount.values.maxOrNull() ?: 0
+    val bestSellers = bookCount.filterValues { it == maxCount }.keys
+    println(bestSellers.minOrNull())
 }
